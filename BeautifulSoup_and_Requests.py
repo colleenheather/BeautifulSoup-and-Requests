97d388f383be78ddebfcc7b7cbbe192c6ee8e8b8
Colleen Ammons <colleen.ammons@gmail.com>
2024-04-10 17:44:42 -0400

    Update BeautifulSoup_and_Requests.py
    adding find()  and find_all()

--- BeautifulSoup_and_Requests.py
+++ BeautifulSoup_and_Requests.py
@@ -7,3 +7,20 @@ soup = BeautifulSoup(page.text, 'html')
 
 print(soup)
 print(soup.prettify())
+
+soup.find('div')
+# only shows very first div tag in html
+
+soup.find_all('div')
+
+soup.find_all('div', class_ = 'col-md-12')
+
+soup.find('p', class_ = 'lead').text.strip()
+# cant use .find_all with .text
+
+soup.find_all('th')
+
+soup.find('th').text.strip()
+
+
+
